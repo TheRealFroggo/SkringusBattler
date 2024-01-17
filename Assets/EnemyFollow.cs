892b602499598468f66a5eae2cc10e48c0ffcece
MatEdwards <matt.ellisedwards@gmail.com>
2024-01-16 19:43:00 -0500

    Enemy Follow + Singleton Pattern (NOT WORKING)

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    private GameObject target;
+    public float speed = 1.5f;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        target = GameManager.Instance.player;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        LookAt2D(transform, target.transform.position);
+    }
+    
+    public static void LookAt2D(Transform transform, Vector2 target)
+    {
+        Vector2 current = transform.position;
+        var direction = target - current;
+        var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!_instance)
+                Debug.LogError("Game Manager is NULL");
+            
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
