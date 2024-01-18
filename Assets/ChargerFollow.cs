1647ef782ea29b8f7f84248ce387eb154273e93b
MatEdwards <matt.ellisedwards@gmail.com>
2024-01-17 22:21:50 -0500

    Shooter Follow Implemented
    Need to clean this up a bit still

--- Assets/ChargerFollow.cs
+++ Assets/ChargerFollow.cs
@@ -18,7 +18,6 @@ public class ChargerFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TEST");
         _target = GameManager.Instance.player;
     }
 
---
+++ Assets/ShooterMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterMove : MonoBehaviour
+{
+    private GameObject _target;
+    public float speed = 1f;
+    public float stopDistance = 10f;
+    public float flightDirection = 1f;
+    
+    private float nextFlightChange = 0;
+    public float flightChangeRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameManager.Instance.player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, _target.transform.position) > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, speed * Time.deltaTime);
+        }else if (Vector2.Distance(transform.position, _target.transform.position) < stopDistance*0.75f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, -speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, (transform.position + transform.right * flightDirection - transform.forward), speed * Time.deltaTime);
+        }
+
+        if (nextFlightChange < Time.time)
+        {
+            flightDirection *= -1;
+            flightChangeRate = Random.Range(1f, 10f);
+            nextFlightChange = Time.time + flightChangeRate;
+        }
+            
+        LookAt2D(transform, _target.transform.position);
+    }
+    
+    private static void LookAt2D(Transform transform, Vector2 target)
+    {
+        Vector2 current = transform.position;
+        var direction = target - current;
+        var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    
+}
