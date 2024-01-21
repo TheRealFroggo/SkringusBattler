ab70dd23b8ca1ea57e5bbd988f3bb090f6fa0074
MatEdwards <matt.ellisedwards@gmail.com>
2024-01-21 16:47:45 -0500

    Adjustments on enemy types (Sprites)

--- Assets/DroneFollow.cs
+++ Assets/DroneFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class DroneFollow : MonoBehaviour
     private GameObject _target;
     public float speed = 1f;
     public float stopDistance = 0.25f;
+    [SerializeField] private float Damage = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,4 @@ public class DroneFollow : MonoBehaviour
         var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg - 90;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-
-    
 }
