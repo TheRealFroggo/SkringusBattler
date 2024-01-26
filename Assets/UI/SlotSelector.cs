5517380fd51e939e32b9bb0ebc3774c4db49bf29
Aidan <aidan.t.park@gmail.com>
2024-01-26 18:27:52 -0500

    Display of status effects bugs fixed

--- Assets/UI/SlotSelector.cs
+++ Assets/UI/SlotSelector.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SlotSelector : MonoBehaviour
 {
     public Slots ActiveDisplaySlot;
-    
-    void OnEnable()
-    {
-        
-    }
+    [SerializeField] public TextMeshPro VirusText;
 
     void Update()
     {
         transform.position = GameManager.Instance.player.transform.position;
+
+        DisplayStats();
+    }
+
+    void DisplayStats()
+    {
+        string displayText = "";
+        foreach(Virus virus in GameManager.Instance.player.GetComponent<PlayerViruses>().Viruses)
+        {
+
+            if (virus.CurrentSlot == ActiveDisplaySlot)
+            {
+                string virusSlot = virus.CurrentVirus.GetVirusID().ToString() + ActiveDisplaySlot.ToString();
+                StatusEffectData effect = GameManager.Instance.GetStatusEffect(virusSlot);
+
+                displayText += virus.CurrentVirus.GetVirusID().ToString() + "\n" + effect.Description + "\n";
+            }
+        }
+
+        VirusText.text = displayText;
+    }
+
+    void OnMouseOver()
+    {
+        VirusText.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        VirusText.enabled = false;
     }
 }
